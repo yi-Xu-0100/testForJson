617238392421e77587d3cd083d92c8cdf6156def
yi_Xu <yi_Xu@yixuju.cn>
2025-02-24 14:20:38 +0800

    添加项目文件。

---
+++ testForJson/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace testForJson;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        string testString = @"{testArray : [2,2]}";
+        var testModel = JsonConvert.DeserializeObject<TestForJsonArray>(testString);
+        InitializeComponent();
+    }
+}
