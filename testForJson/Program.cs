8e8da3c1345d4af386be3ceb32b1ec18babae856
yi_Xu <yi_Xu@yixuju.cn>
2025-02-24 14:35:51 +0800

    用控制台做示例

---
+++ testForJson/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace testForJson;
+
+class TestForJsonArray
+{
+    public List<double> testArray = [1, 1];
+}
+
+public class Program()
+{
+    static void Main()
+    {
+        string testString = @"{testArray : [2,2]}";
+        Console.WriteLine($"{nameof(testString)}: {testString}");
+        var testModel = JsonConvert.DeserializeObject<TestForJsonArray>(testString);
+
+        string testModelString = JsonConvert.SerializeObject(testModel);
+        Console.WriteLine($"{nameof(testModelString)}: {testModelString}");
+    }
+}
